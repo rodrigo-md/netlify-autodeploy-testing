69943e867685c28e8c262cdc9ce127e600b7f893
Rodrigo Martinez <rodrigo.martinez.diaz@autodesk.com>
2022-02-19 01:50:13 -0300

    init project

---
+++ index.js
@@ -0,0 +1,46 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+const template = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main project website</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+</body>
+</html>
+`
+
+async function wait(ms) {
+    let timeoutId;
+    return new Promise((resolve, _) => {
+        timeoutId = setTimeout(resolve, ms);
+    }).finally(() => clearTimeout(timeoutId));
+}
+
+
+async function build() {
+    try {
+    console.log('Building project...');
+    await wait(3000);
+    await fs.mkdir(path.join(__dirname, 'public'), { recursive: true });
+    console.log(path.join(__dirname, 'public', 'index.html'))
+    await fs.writeFile(path.join(__dirname, 'public', 'index.html'), template);
+    console.log('Project created successfully');
+    } catch(err) {
+        console.log('Unable to create project');
+        console.error(err);
+    }
+}
+
+
+
+
+
+
+build();
